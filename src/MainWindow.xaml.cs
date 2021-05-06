11aaa4decf421005bb4efb6e342be79a41d9712b
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-05-07 08:18:54 +0900

    feat: 기초 UI 구현

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OthelloSharp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ControllBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        
+        private void MenuEmp_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Opacity = 1;
+        }
+
+        private void MenuEmp_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Opacity = 0;
+        }
+
+        private void CloseButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
