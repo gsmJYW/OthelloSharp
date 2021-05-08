10e37cb74fd69b4475fa88ab196dd9c326537e26
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-05-08 20:17:42 +0900

    feat: tcp 통신 구현

---
+++ src/Client.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace OthelloSharp
+{
+	public class Client
+	{
+		public class AsyncObject
+		{
+			public byte[] Buffer;
+			public Socket WorkingSocket;
+			public AsyncObject(int bufferSize)
+			{
+				Buffer = new byte[bufferSize];
+			}
+		}
+
+		private MainWindow mainWindow;
+		private Socket ClientSocket = null;
+		private AsyncCallback ReceiveHandler;
+		private AsyncCallback SendHandler;
+
+		public Client(MainWindow mainWindow)
+		{
+			this.mainWindow = mainWindow;
+
+			ReceiveHandler = new AsyncCallback(HandleDataReceive);
+			SendHandler = new AsyncCallback(HandleDataSend);
+		}
+
+		public bool IsServerConnected()
+		{
+			return ClientSocket.Poll(1000, SelectMode.SelectRead) && (ClientSocket.Available == 0);
+		}
+
+		public void Connect(string serverAddress, ushort port)
+		{
+			ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			ClientSocket.Connect(serverAddress, port);
+
+			AsyncObject ao = new AsyncObject(4096);
+			ao.WorkingSocket = ClientSocket;
+
+			ClientSocket.BeginReceive(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, ReceiveHandler, ao);
+		}
+
+		public void Close()
+		{
+			ClientSocket.Close();
+		}
+
+		public void SendMessage(string message)
+		{
+			AsyncObject ao = new AsyncObject(1);
+			ao.Buffer = Encoding.Unicode.GetBytes(message);
+			ao.WorkingSocket = ClientSocket;
+
+			ClientSocket.BeginSend(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, SendHandler, ao);
+		}
+
+		private void HandleDataReceive(IAsyncResult ar)
+		{
+			AsyncObject ao = (AsyncObject)ar.AsyncState;
+
+			int recvBytes;
+
+			try
+			{
+				recvBytes = ao.WorkingSocket.EndReceive(ar);
+			}
+			catch
+            {
+				mainWindow.Disconnected();
+				return;
+            }
+
+			if (recvBytes > 0)
+			{
+				byte[] msgByte = new byte[recvBytes];
+				Array.Copy(ao.Buffer, msgByte, recvBytes);
+				MessageBox.Show(Encoding.Unicode.GetString(msgByte));
+			}
+			ao.WorkingSocket.BeginReceive(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, ReceiveHandler, ao);
+		}
+
+		private void HandleDataSend(IAsyncResult ar)
+		{
+			AsyncObject ao = (AsyncObject)ar.AsyncState;
+			int sentBytes = ao.WorkingSocket.EndSend(ar);
+
+			if (sentBytes > 0)
+			{
+				byte[] msgByte = new byte[sentBytes];
+				Array.Copy(ao.Buffer, msgByte, sentBytes);
+				MessageBox.Show(Encoding.Unicode.GetString(msgByte));
+			}
+		}
+	}
+}
---
+++ src/Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace OthelloSharp
+{
+	public class Server
+	{
+		public class AsyncObject
+		{
+			public byte[] Buffer;
+			public Socket WorkingSocket;
+			public AsyncObject(int bufferSize)
+			{
+				Buffer = new byte[bufferSize];
+			}
+		}
+
+		private MainWindow mainWindow;
+		private Socket ConnectedClient = null;
+		private Socket ServerSocket = null;
+		private AsyncCallback ReceiveHandler;
+		private AsyncCallback SendHandler;
+		private AsyncCallback AcceptHandler;
+
+		public Server(MainWindow mainWindow)
+		{
+			this.mainWindow = mainWindow;
+
+			ReceiveHandler = new AsyncCallback(HandleDataReceive);
+			SendHandler = new AsyncCallback(HandleDataSend);
+			AcceptHandler = new AsyncCallback(HandleClientConnectionRequest);
+		}
+
+		public bool IsClientConnected()
+		{
+			return ServerSocket.Poll(1000, SelectMode.SelectRead) && (ServerSocket.Available == 0);
+		}
+
+		public void Open(ushort port)
+		{
+			ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			ServerSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+			ServerSocket.Listen(5);
+			ServerSocket.BeginAccept(AcceptHandler, null);
+		}
+
+		public void Close()
+		{
+			ServerSocket.Close();
+		}
+
+		public void SendMessage(string message)
+		{
+			AsyncObject ao = new AsyncObject(1);
+			ao.Buffer = Encoding.Unicode.GetBytes(message);
+			ao.WorkingSocket = ConnectedClient;
+
+			ConnectedClient.BeginSend(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, SendHandler, ao);
+		}
+
+		private void HandleClientConnectionRequest(IAsyncResult ar)
+		{
+			Socket sockClient;
+
+			try
+			{
+				sockClient = ServerSocket.EndAccept(ar);
+			}
+			catch
+            {
+				return;
+            }
+
+			AsyncObject ao = new AsyncObject(4096);
+			ao.WorkingSocket = sockClient;
+			ConnectedClient = sockClient;
+
+			sockClient.BeginReceive(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, ReceiveHandler, ao);
+		}
+
+		private void HandleDataReceive(IAsyncResult ar)
+		{
+			AsyncObject ao = (AsyncObject)ar.AsyncState;
+			int recvBytes;
+
+			try
+			{
+				recvBytes = ao.WorkingSocket.EndReceive(ar);
+			}
+			catch
+            {
+				mainWindow.Disconnected();
+				return;
+            }
+
+			if (recvBytes > 0)
+			{
+				byte[] msgByte = new byte[recvBytes];
+				Array.Copy(ao.Buffer, msgByte, recvBytes);
+				MessageBox.Show(Encoding.Unicode.GetString(msgByte));
+			}
+
+			ao.WorkingSocket.BeginReceive(ao.Buffer, 0, ao.Buffer.Length, SocketFlags.None, ReceiveHandler, ao);
+		}
+		private void HandleDataSend(IAsyncResult ar)
+		{
+			AsyncObject ao = (AsyncObject)ar.AsyncState;
+            int sentBytes = ao.WorkingSocket.EndSend(ar);
+
+			if (sentBytes > 0)
+			{
+				byte[] msgByte = new byte[sentBytes];
+				Array.Copy(ao.Buffer, msgByte, sentBytes);
+				MessageBox.Show(Encoding.Unicode.GetString(msgByte));
+			}
+		}
+	}
+}
